97620f9ee90977dfa3ea790ea218af1e6a7ff9a4
Rudrresh <97274993+Rudrresh@users.noreply.github.com>
2024-03-27 22:22:57 +0530

    Add 1754

--- introductory-problems/1754.cpp
+++ introductory-problems/1754.cpp
@@ -1,6 +1,4 @@
 #include <bits/stdc++.h>
-#include <tuple>
-#include <unordered_map>
 typedef long long ll;
 
 using namespace std;
@@ -12,12 +10,8 @@ int main() {
     while (t--) {
         long long a, b;
         cin >> a >> b;
-        if (a == 0 and b == 0) cout << "YES" << endl;
-        else if (a == 0 or b == 0) cout << "NO" << endl;
-        else if ((a == 1 and b == 1) or (a == 2 and b == 2)) cout << "NO" << endl;
-        else if (!(2 * a - b) % 3) cout << "YES" << endl;
-        else if (!(2 * b - a) % 3) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        if ((a + b) % 3 != 0 or (b > 2*a) or (a > 2*b)) cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
 
 
